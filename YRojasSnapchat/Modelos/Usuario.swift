b53ca242c2670deee6793e545c51d749e4195f54
yrojas <yesica.rojas@tecsup.edu.pe>
2023-11-14 20:12:35 -0500

    no message

---
+++ YRojasSnapchat/Modelos/Usuario.swift
@@ -0,0 +1,13 @@
+//
+//  Usuario.swift
+//  YRojasSnapchat
+//
+//  Created by yrojas on 14/11/23.
+//
+
+import Foundation
+
+class Usuario{
+    var email = ""
+    var uid = ""
+}
--- YRojasSnapchat/ViewControllers/IniciarSesionViewController.swift
+++ YRojasSnapchat/ViewControllers/IniciarSesionViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 import FirebaseAuth
 import FirebaseCore
 import GoogleSignIn
+import FirebaseDatabase
 
 class IniciarSesionViewController: UIViewController {
 
@@ -56,8 +57,27 @@ class IniciarSesionViewController: UIViewController {
         Auth.auth().signIn(withEmail: emailTextField.text!, password: passwordTextField.text!){ (user, error) in print("Intentar iniciar sesion")
             if error != nil{
                 print("Se presento el siguiente error: \(error)")
+                Auth.auth().createUser(withEmail: self.emailTextField.text!, password: self.passwordTextField.text!, completion: {(user,error) in print("Intentando crear un usuario")
+                    if error != nil{
+                        print("Se presento el siguiente error al crear el usuario: \(error)")
+                    }else{
+                        print("El usuario fue credo exitosamente")
+                        Database.database().reference().child("usuarios").child(user!.user.uid).child("email").setValue(user!.user.email)
+                        
+                        let alerta = UIAlertController(title: "Creacion de Usuario", message:
+                                                        "Usuario: \(self.emailTextField.text!) se creo correctamente.",
+                        preferredStyle:
+                        .alert)
+                        let btnOK = UIAlertAction(title: "Aceptar", style: .default, handler:
+                        { (UIAlertAction) in self.performSegue(withIdentifier: "iniciarsesionsegue", sender: nil)
+                        })
+                        alerta.addAction(btnOK)
+                        self.present(alerta, animated: true, completion: nil)
+                    }
+                })
             }else{
                 print("Inicio de sesion de sesion exitoso")
+                self.performSegue(withIdentifier: "iniciarsesionsegue", sender: nil)
             }
         }
     }
